3704bd288fb1c9c1cf6f73233e598cf72f0df6a1
Jan Formanek <formanek@teco.edu>
2019-03-26 16:05:21 +0100

    Korrekturen am shiftregister durchgeführt. Defines für Verkabelung angelegt

--- shift_register_test/shift_register_test.ino
+++ shift_register_test/shift_register_test.ino
@@ -2,13 +2,78 @@
 #define CLK_595 13 
 #define RCLK_595 12 //the latch pin, to transfer from buffer to output
 
+#define MUX_X1 17 
+#define MUX_X2 23 
+
 //achtung invertiert in meinem fall
 //achtung max 50Khz
 
+int portExpander = 0x00;
 
-/*void enableMT3608() {
+#define EXT1_OUT 0
+#define LOAD100MA_EN 1
+#define POWERBANK_EN 2
+#define MT3608_EN 3
+#define MUX4052B_S1 4
+#define MUX4052B_S0 5
+#define MUX4052B_EN 6
+#define EXT2_OUT 7
+#define A7_PWR_KEY 8
+#define A7_MODULE_EN 9
+#define MUX4052A_S1 10
+#define MUX4052A_S0 11
+#define MUX4052A_EN 12
+#define A7_SLEEP 13
+#define A7_RESET 14
+#define SDS011_EN 15
+
+void updatePorts() {
+  shiftoutSlow(CLK_595, RCLK_595, SERIN_595, highByte(portExpander));
+  shiftoutSlow(CLK_595, RCLK_595, SERIN_595, lowByte(portExpander));
+  latch(RCLK_595);
+}
+
+void enableMT3608() {
+  portExpander = portExpander & (0 << MT3608_EN); 
+  updatePorts();
   
-}*/
+}
+
+void disableMT3608() {
+  portExpander = portExpander | (1 << MT3608_EN);
+  updatePorts();
+}
+
+void setMuxA(byte channel) {
+  switch(channel) {
+    case 0:
+      portExpander = portExpander & ~(1 << MUX4052A_S0);
+      portExpander = portExpander & ~(1 << MUX4052A_S1);
+      break;
+   case 1:
+      portExpander = portExpander | (1 << MUX4052A_S0);
+      portExpander = portExpander & ~(1 << MUX4052A_S1);
+      break;
+   case 2:
+     portExpander = portExpander & ~(1 << MUX4052A_S0);
+     portExpander = portExpander | (1 << MUX4052A_S1);
+     break;
+   case 3:
+    portExpander = portExpander | (1 << MUX4052A_S0);
+     portExpander = portExpander | (1 << MUX4052A_S1);
+     break;
+  }
+
+  updatePorts();
+}
+
+void resetPorts() {
+  //portExpander = 0xFFFF;
+  portExpander = 0x0000;
+  updatePorts();
+}
+
+
 
 byte reverseByte(byte data_) {
   byte temp = 0x00;
@@ -29,11 +94,15 @@ byte reverseByte(byte data_) {
 
 //testen ob man 16bit mit 2xigem Aufruf ausschiften kann
 void shiftoutSlow(byte clkPin, byte  rclkPin, byte dataPin, byte data_) {
+  //invert data, cause dataline is inverted
+  data_ = ~data_;
+  data_ = reverseByte(data_);
   pinMode(dataPin, OUTPUT);
   pinMode(clkPin, OUTPUT);
   pinMode(rclkPin, OUTPUT); 
 
   //idle clock = low
+  digitalWrite(rclkPin, HIGH); //relase latchout
   digitalWrite(clkPin, HIGH);
   digitalWrite(rclkPin, HIGH); //latch, call at end
 
@@ -51,13 +120,9 @@ void shiftoutSlow(byte clkPin, byte  rclkPin, byte dataPin, byte data_) {
     digitalWrite(clkPin, HIGH); //clk release
     data_ = data_ >> 1;
   }
-  
-  digitalWrite(rclkPin, LOW); //latchout data
-  delay(1);
-  digitalWrite(rclkPin, HIGH); //relase latchout
 }
 
-void latch() {
+void latch(byte rclkPin) {
   digitalWrite(rclkPin, LOW); //latchout data
   delay(1);
   digitalWrite(rclkPin, HIGH); //relase latchout
@@ -76,14 +141,27 @@ void setup() {
   //reversedtestbyte =123;
   Serial.print("testbyte:"+String(testbyte2)); 
   Serial.print("shiftedbyte:"+String(shiftedbyte)); 
+  resetPorts();
   
 }
 
 void loop() {
   //shiftoutSlow(CLK_595, RCLK_595, SERIN_595, 0x00);
-  shiftoutSlow(CLK_595, RCLK_595, SERIN_595, B00000000);
+  /*shiftoutSlow(CLK_595, RCLK_595, SERIN_595, B00000000);
   shiftoutSlow(CLK_595, RCLK_595, SERIN_595, B11111111);
- 
+  latch(RCLK_595);*/
+  //enableMT3608();
+  setMuxA(1);
+  //portExpander = portExpander | (1 << MUX4052A_S0);
+  //portExpander = portExpander & (0 << MUX4052A_S1);
+  //portExpander = 0x8000;
+  //portExpander = 0x4000;
+  //portExpander = 0x2000;
+  //updatePorts();
+  //disableMT3608();
+  /*shiftoutSlow(CLK_595, RCLK_595, SERIN_595, B00000010); //15-8
+  shiftoutSlow(CLK_595, RCLK_595, SERIN_595, B00001000); //7-0
+  latch(RCLK_595);*/
   
   
   
